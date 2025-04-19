07c7c8d87968dce87feed23f9f219685a07f610c
parthanand4054 <parthanand4054@gmail.com>
2025-04-18 20:31:43 -0500

    Added docstrings and api related files

---
+++ RickAndMorty/APIClient/RMRequest.swift
@@ -0,0 +1,13 @@
+//
+//  RMRequest.swift
+//  RickAndMorty
+//
+//  Created by Parth Anand on 18/04/25.
+//
+
+import Foundation
+
+/// Object that represents a single API call
+final class RMRequest {
+    
+}
